9576e779f924efa289dd9ee679eb569245686093
Browny-123 <andybrown098@live.co.uk>
2020-02-05 16:53:22 +0100

    Updates

---
+++ precache-manifest.9e3c505b47a420822dd838a5849bbcb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "558b9ee73e572236bcca9645caa8efd1",
+    "url": "/Diasporeines/index.html"
+  },
+  {
+    "revision": "324caff7c4c6c33b8764",
+    "url": "/Diasporeines/static/css/main.5e16478e.chunk.css"
+  },
+  {
+    "revision": "4d35656f5f3aea5fa6a8",
+    "url": "/Diasporeines/static/js/2.de18b09c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Diasporeines/static/js/2.de18b09c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "324caff7c4c6c33b8764",
+    "url": "/Diasporeines/static/js/main.8b8bc4d9.chunk.js"
+  },
+  {
+    "revision": "37ec56aefd81bcaece8d",
+    "url": "/Diasporeines/static/js/runtime-main.df569a1d.js"
+  }
+]);
